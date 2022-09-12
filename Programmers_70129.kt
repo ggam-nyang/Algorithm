3a0c2d1537467f8ac2e5a983e4925192ea2c41a3
ggam-nyang <v44ads@naver.com>
2022-09-12 22:04:10 +0900

    algorithm solved

---
+++ Programmers_70129.kt
@@ -0,0 +1,28 @@
+class Solution70129 {
+    fun solution(s: String): IntArray {
+        var deleteCnt = 0
+        var convertCnt = 0
+        var currentStr = s
+
+        while (currentStr.length != 1) {
+            val convertResult = convert(currentStr)
+            convertCnt ++
+            deleteCnt += convertResult.first
+            currentStr = convertResult.second
+        }
+
+        var answer: IntArray = intArrayOf(convertCnt, deleteCnt)
+        return answer
+    }
+
+    private fun convert(s: String): Pair<Int, String> {
+        var deleteCnt = 0
+        s.forEach {
+            if (it == '0') deleteCnt ++
+        }
+
+        val convertStr = s.filter { it == '1' }.length.toString(2)
+
+        return Pair(deleteCnt, convertStr)
+    }
+}
