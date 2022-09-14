ebae559a589a3b29d67992c30c18add537c59478
ggam-nyang <v44ads@naver.com>
2022-09-14 15:19:45 +0900

    algorithm solved

---
+++ Programmers_87390.kt
@@ -0,0 +1,14 @@
+import kotlin.math.max
+import kotlin.math.sqrt
+
+class Programmers87390 {
+    fun solution(n: Int, left: Long, right: Long): ArrayList<Long> {
+        val flattenArray = arrayListOf<Long>()
+        for (i in left .. right) {
+            flattenArray.add(max(i / n.toLong() + 1L, i % n.toLong() + 1L))
+        }
+
+        return flattenArray
+    }
+
+}
