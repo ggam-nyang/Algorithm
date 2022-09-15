9006b27c3e8f03526c473ad5de8b92ca85e4ab98
ggam-nyang <v44ads@naver.com>
2022-09-15 10:21:40 +0900

    algorithm solved

---
+++ Programmers12985.kt
@@ -0,0 +1,19 @@
+import kotlin.math.abs
+import kotlin.math.sqrt
+
+class Programmers12985 {
+    fun solution(n: Int, a: Int, b: Int): Int {
+        var orderA = (a + 1) / 2
+        var orderB = (b + 1) / 2
+
+        var answer = 1
+
+        while (abs(orderA - orderB) > 0) {
+            orderA = (orderA + 1) / 2
+            orderB = (orderB + 1) / 2
+            answer ++
+        }
+
+        return answer
+    }
+}
