3a6cf58af8289f22053e5c01ded3238e81cd009d
ggam-nyang <v44ads@naver.com>
2022-09-24 10:20:59 +0900

    algorithm solved

---
+++ Programmers77484.kt
@@ -0,0 +1,20 @@
+import kotlin.math.abs
+import kotlin.math.sqrt
+
+class Programmers77484 {
+    fun solution(lottos: IntArray, win_nums: IntArray): IntArray {
+        val maxNum = lottos.filterNot { it == 0 }
+            .count { win_nums.contains(it) } + lottos.count { it == 0 }
+
+        val minNum = lottos.filterNot { it == 0 }
+            .count { win_nums.contains(it) }
+
+        return intArrayOf(convertToLottoRank(maxNum), convertToLottoRank(minNum))
+    }
+
+    fun convertToLottoRank(correctCnt: Int): Int {
+        val answer = 7 -correctCnt
+
+        return if (answer > 6) 6 else answer
+    }
+}
