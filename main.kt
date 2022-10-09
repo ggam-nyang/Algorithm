e1649f08403b89e56ab0efe23f45d1ec463e00d0
ggam-nyang <v44ads@naver.com>
2022-10-09 13:59:47 +0900

    algo solved

--- main.kt
+++ main.kt
@@ -1,7 +1,20 @@
-fun main() {
-    val test = IntArray(4) { it -> it * 1}
+import java.util.PriorityQueue
 
-    test.forEach {
-        println(it)
-    }
+fun main() {
+    var test = PriorityQueue<Int>(5)
+
+    test.add(2)
+    test.add(1)
+    test.add(3)
+    test.add(-3)
+    test.add(19213)
+
+
+    println(test)
+    test.remove(test.last())
+    test.poll()
+    test.reversed()
+
+
+    println(test)
 }
