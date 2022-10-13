b959cf56ad476796d322b33211759b600d2b9a0b
ggam-nyang <v44ads@naver.com>
2022-10-14 00:16:29 +0900

    algo solved

--- Programmers42626.java
+++ Programmers42626.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.stream.Collectors;
 
-class Solution {
+class Programmers42626 {
     public int solution(int[] scoville, int K) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         pq.addAll(Arrays.stream(scoville).boxed().collect(Collectors.toList()));
