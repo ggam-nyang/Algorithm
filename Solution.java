aa1843ea3041f3331112818efe61fd72a7afd53a
ggam-nyang <v44ads@naver.com>
2022-10-19 23:45:28 +0900

    algo solved

---
+++ Solution.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        HashSet<String> hs = new HashSet<>();
+        for (String s : phone_book) {
+            for (int i = 1; i < s.length(); i++) {
+                hs.add(s.substring(0, i));
+            }
+        }
+
+        for (String s : phone_book) {
+            if (hs.contains(s)) return false;
+        }
+
+        return true;
+    }
+}
