30c1c138489c0f075dc43830f090a56448555c19
ggam-nyang <v44ads@naver.com>
2022-10-24 23:12:19 +0900

    algo solved

--- fLab/LeetCode1481.kt
+++ fLab/LeetCode1481.kt
@@ -1,12 +1,16 @@
 package fLab
 
-class Solution {
+import java.util.PriorityQueue
+
+class LeetCode1481 {
     fun findLeastNumOfUniqueInts(arr: IntArray, k: Int): Int {
+        // faster by heapify
         val hm = arr.groupBy { it }
             .mapValues { it.value.size }
 
         var copyK = k
         var leftNum = hm.size
+
         hm.values
             .sorted()
             .forEach { cnt ->
