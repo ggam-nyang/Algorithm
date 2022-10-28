836189ab38686d32faeac7203c457e8953752f68
ggam-nyang <v44ads@naver.com>
2022-10-28 13:41:40 +0900

    algo solved

--- Programmers131701.kt
+++ Programmers131701.kt
@@ -1,12 +1,13 @@
 class Programmers131701 {
     fun solution(elements: IntArray): Int {
         val sumSet = hashSetOf<Int>()
+
+        val newElements = elements + elements
         elements.forEachIndexed { i, firstValue ->
             var num = firstValue
             sumSet.add(num)
-            (i + 1 until elements.size).forEachIndexed { j, nextValue ->
-                println("$i    $nextValue")
-                num += elements[nextValue]
+            (i + 1 until i + elements.size).forEachIndexed { j, nextValue ->
+                num += newElements[nextValue]
                 sumSet.add(num)
             }
         }
