1ce235c12b5a55b48516b133e1f1e1bbf1f1595a
ggam-nyang <v44ads@naver.com>
2022-11-05 14:38:13 +0900

    algo solved, but not so good

---
+++ fLab/LeetCode938.kt
@@ -0,0 +1,24 @@
+package fLab
+
+class Solution {
+    var sumOfTree = 0
+
+    fun rangeSumBST(root: TreeNode?, low: Int, high: Int): Int {
+        traverse(low, high, root)
+
+        return sumOfTree
+    }
+
+    // change if block then can reduce traverse line
+    private fun traverse(low: Int, high: Int, aNode: TreeNode?) {
+        if (aNode == null) return
+
+        if (aNode.`val` < low) traverse(low, high, aNode.right)
+        else if (aNode.`val` > high) traverse(low, high, aNode.left)
+        else {
+            sumOfTree += aNode.`val`
+            traverse(low, high, aNode.right)
+            traverse(low, high, aNode.left)
+        }
+    }
+}
