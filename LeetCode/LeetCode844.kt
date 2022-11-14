eeff93c61f4c8398a08da7e537779930896f4a64
ggam-nyang <v44ads@naver.com>
2022-11-14 14:33:16 +0900

    need to reduce memory

---
+++ LeetCode/LeetCode844.kt
@@ -0,0 +1,25 @@
+package LeetCode
+
+class Solution {
+    fun backspaceCompare(s: String, t: String): Boolean {
+        val newS = deleteHash(0, s, "")
+        val newT = deleteHash(0, t, "")
+
+        println("$newS,  $newT")
+
+        return newS == newT
+    }
+
+    private fun deleteHash(hashCnt: Int, str: String, alpha: String): String {
+        if (str == "") return alpha
+        if (hashCnt >= str.length)
+            return alpha
+
+        return if (str.last() != '#') {
+            if (hashCnt == 0)
+                deleteHash(hashCnt , str.dropLast(1), str.last() + alpha)
+            else deleteHash(hashCnt - 1, str.dropLast(1), alpha)
+        }
+        else deleteHash(hashCnt + 1, str.dropLast(1), alpha)
+    }
+}
