b939120cf3473729d880f707af0e72218b724407
ggam-nyang <v44ads@naver.com>
2023-03-17 23:19:04 +0900

    algo studying..

---
+++ baekjoon/17182.kt
@@ -0,0 +1,11 @@
+package baekjoon
+
+import java.util.*
+
+private lateinit var graph: Array<IntArray>
+
+fun main() = with(Scanner(System.`in`)) {
+    val N = nextLine().toInt()
+    val M = nextLine().toInt()
+
+}
