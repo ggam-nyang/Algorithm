95c0d1e38394fd6a76f8898b55b06ea80dfc4cc6
ggam-nyang <v44ads@naver.com>
2023-08-05 23:49:38 +0900

    good evening algo

---
+++ leetCode/topInterview150/array/27.kt
@@ -0,0 +1,28 @@
+package leetCode.topInterview150.array
+
+class Solution27 {
+    fun removeElement(nums: IntArray, `val`: Int): Int {
+        var k = 0
+        val tempList = arrayListOf<Int>()
+
+        nums.forEachIndexed { i, num ->
+            if (num != `val`) {
+                tempList.add(num)
+                k++
+            }
+        }
+
+        tempList.indices.forEach {
+            nums[it] = tempList[it]
+        }
+
+        return k
+    }
+
+    fun removeElementBeautiful(nums: IntArray, num: Int): Int {
+        var counter = 0
+        nums.forEach { if (it != num) nums[counter++] = it }
+
+        return counter
+    }
+}
