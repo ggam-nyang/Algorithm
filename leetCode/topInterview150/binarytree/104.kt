cf0edadf27c84111394bcce651fd3d4a55995b18
ggam-nyang <v44ads@naver.com>
2023-08-15 13:28:54 +0900

    Good afternoon algo

---
+++ leetCode/topInterview150/binarytree/104.kt
@@ -0,0 +1,26 @@
+package leetCode.topInterview150.binarytree
+
+import kotlin.math.max
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution104 {
+    fun maxDepth(root: TreeNode?): Int {
+        if (root == null) return 0
+
+        return 1 + max(maxDepth(root.left), maxDepth(root.right))
+    }
+}
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
