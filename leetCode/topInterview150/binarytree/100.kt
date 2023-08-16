1c128d661c7edda8add0a92dcf6b7b3c56a87901
ggam-nyang <v44ads@naver.com>
2023-08-16 23:31:02 +0900

    Good night algo

---
+++ leetCode/topInterview150/binarytree/100.kt
@@ -0,0 +1,21 @@
+package leetCode.topInterview150.binarytree
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution100 {
+    fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+        if (p == null && q == null) return true
+        if (p?.`val` != q?.`val`) return false
+
+        if (isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)) return true
+        return false
+    }
+}
