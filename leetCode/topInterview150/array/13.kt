65f28dafff28a5afdff34de23744cded6b15bc99
ggam-nyang <v44ads@naver.com>
2023-09-01 23:54:44 +0900

    Good night algo

---
+++ leetCode/topInterview150/array/13.kt
@@ -0,0 +1,18 @@
+package leetCode.topInterview150.array
+
+class Solution13 {
+    private val romanMap =
+        hashMapOf(
+            'I' to 1,
+            'V' to 5,
+            'X' to 10,
+            'L' to 50,
+            'C' to 100,
+            'D' to 500,
+            'M' to 1000
+            )
+
+    fun romanToInt(s: String): Int {
+
+    }
+}
