16c5a6f5eca532732523dbe86c46ca3abb1d49b8
ggam-nyang <v44ads@naver.com>
2023-09-13 23:48:03 +0900

    Good afternoon algo

---
+++ leetCode/topInterview150/binarytree/222.kt
@@ -0,0 +1,23 @@
+package leetCode.topInterview150.binarytree
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution222 {
+    private var count = 0
+    fun countNodes(root: TreeNode?): Int {
+        if (root == null) return 0
+        count++
+        countNodes(root.left)
+        countNodes(root.right)
+
+        return count
+    }
+}
