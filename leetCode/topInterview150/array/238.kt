3a31af69e8ad15989f946cd49eb6fec2bfe9a029
ggam-nyang <v44ads@naver.com>
2023-09-14 14:36:45 +0900

    Good afternoon algo

---
+++ leetCode/topInterview150/array/238.kt
@@ -0,0 +1,20 @@
+package leetCode.topInterview150.array
+
+class Solution238 {
+    fun productExceptSelf(nums: IntArray): IntArray {
+        val answer = IntArray(nums.size) { 1 }
+        var n = 1
+
+        nums.indices.forEach {
+            answer[it] = n
+            n *= nums[it]
+        }
+        n = 1
+        nums.indices.reversed().forEach {
+            answer[it] *= n
+            n *= nums[it]
+        }
+
+        return answer
+    }
+}
