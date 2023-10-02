d56dac4c89e15206e6287f938b68120fedf78707
ggam-nyang <v44ads@naver.com>
2023-10-03 00:05:49 +0900

    Good night algo

--- leetCode/topInterview150/math/66.kt
+++ leetCode/topInterview150/math/66.kt
@@ -2,6 +2,18 @@ package leetCode.topInterview150.math
 
 class Solution66 {
     fun plusOne(digits: IntArray): IntArray {
+        if (digits.all { it == 9 })
+            return intArrayOf(1) + IntArray(digits.size)
 
+        digits[digits.lastIndex] += 1
+        for (i in digits.indices.reversed()) {
+            if (digits[i] == 10) {
+                digits[i] = 0
+                digits[i - 1] += 1
+            }
+            else return digits
+        }
+
+        return digits
     }
 }
